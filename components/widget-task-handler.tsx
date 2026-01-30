8100ad021b0350ee5263cc6bc2339ec72e125616
gizag889 <gizaguri0426@outlook.jp>
2026-01-31 07:03:22 +0900

    [fix]case文ロジック変更

--- components/widget-task-handler.tsx
+++ components/widget-task-handler.tsx
@@ -4,16 +4,18 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { WidgetView } from '../components/WidgetView';
 
 export async function widgetTaskHandler(props: WidgetTaskHandlerProps) {
-  const { clickAction, renderWidget } = props;
+  // WidgetTaskHandlerPropsからclickAction renderWidgetを分割代入で取り出す
+  //これにより、以降の行では props.renderWidget(...) のように書く代わりに、単に renderWidget(...) と記述できるようになっています
+  const { clickAction, renderWidget, widgetAction } = props;
   // システムからの更新要求やウィジェット表示時に呼ばれる
-  if (props.widgetAction === 'WIDGET_UPDATE' || props.widgetAction === 'WIDGET_ADDED') {
+  if (widgetAction === 'WIDGET_UPDATE' || widgetAction === 'WIDGET_ADDED') {
     // 保存されている最新のテキストを取得
     const savedText = await AsyncStorage.getItem('latest_notion_text');
     
     // ウィジェットを再描画
     renderWidget(<WidgetView content={savedText || 'メモがありません'} />);
   }
-  switch (props.widgetAction) {
+  switch (widgetAction) {
     case "WIDGET_UPDATE":
     case "WIDGET_ADDED":
 
