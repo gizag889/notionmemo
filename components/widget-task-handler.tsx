f5c4cca5836d3abbc16f802cdf5b071d3c798974
gizag889 <gizaguri0426@outlook.jp>
2026-01-31 19:04:58 +0900

    [fix]case文ロジック変更

--- components/widget-task-handler.tsx
+++ components/widget-task-handler.tsx
@@ -1,37 +1,38 @@
 "use no memo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Linking } from "react-native";
 import type { WidgetTaskHandlerProps } from "react-native-android-widget";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { WidgetView } from '../components/WidgetView';
+import { WidgetView } from "../components/WidgetView";
 
 export async function widgetTaskHandler(props: WidgetTaskHandlerProps) {
   // WidgetTaskHandlerPropsからclickAction renderWidgetを分割代入で取り出す
   //これにより、以降の行では props.renderWidget(...) のように書く代わりに、単に renderWidget(...) と記述できるようになっています
   const { clickAction, renderWidget, widgetAction } = props;
   // システムからの更新要求やウィジェット表示時に呼ばれる
-  if (widgetAction === 'WIDGET_UPDATE' || widgetAction === 'WIDGET_ADDED') {
+  if (widgetAction === "WIDGET_UPDATE" || widgetAction === "WIDGET_ADDED") {
     // 保存されている最新のテキストを取得
-    const savedText = await AsyncStorage.getItem('latest_notion_text');
-    
+    const savedText = await AsyncStorage.getItem("latest_notion_text");
+
     // ウィジェットを再描画
-    renderWidget(<WidgetView content={savedText || 'メモがありません'} />);
+    renderWidget(<WidgetView content={savedText || "メモがありません"} />);
   }
   switch (widgetAction) {
     case "WIDGET_UPDATE":
     case "WIDGET_ADDED":
-
-        const savedText = await AsyncStorage.getItem('latest_notion_text');
-        renderWidget(<WidgetView content={savedText || 'メモがありません'} />);
-        break;
+      const savedText = await AsyncStorage.getItem("latest_notion_text");
+      renderWidget(<WidgetView content={savedText || "メモがありません"} />);
+      break;
     case "WIDGET_CLICK":
       if (clickAction === "OPEN_MAIN") {
         // Do stuff when primitive with `clickAction="MY_ACTION"` is clicked
         // props.clickActionData === { id: 0 }
+        Linking.openURL("notionmemo://");
       }
-      if (clickAction === "OPEN_INPUT") {
-        // Do stuff when primitive with `clickAction="MY_ACTION"` is clicked
-        // props.clickActionData === { id: 0 }
-      }
-      break;
+    // if (clickAction === "OPEN_INPUT") {
+    //   // Do stuff when primitive with `clickAction="MY_ACTION"` is clicked
+    //   // props.clickActionData === { id: 0 }
+    // }
+    // break;
     default:
       break;
   }
