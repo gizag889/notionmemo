206c8bb77bbd12d4c439a56ec68c232e1335b192
gizag889 <gizaguri0426@outlook.jp>
2026-02-01 01:42:26 +0900

    [add]requestwidgetロジック追加

---
+++ lib/widget.tsx
@@ -0,0 +1,12 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React from "react";
+import { requestWidgetUpdate } from "react-native-android-widget";
+import { WidgetView } from "../components/WidgetView";
+
+export const updateWidgetContent = async (content: string) => {
+  await AsyncStorage.setItem("latest_notion_text", content);
+  requestWidgetUpdate({
+    widgetName: "NotionClipboardWidget",
+    renderWidget: () => <WidgetView content={content} />,
+  });
+};
