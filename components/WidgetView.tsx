484e43c56d02f20a338de1322d77ea3dfc443a47
gizag889 <gizaguri0426@outlook.jp>
2026-02-04 01:15:33 +0900

    chachanges

--- components/WidgetView.tsx
+++ components/WidgetView.tsx
@@ -3,6 +3,7 @@
 import {
   FlexWidget,
   ListWidget,
+  OverlapWidget,
   TextWidget,
 } from "react-native-android-widget";
 
@@ -24,33 +25,80 @@ export function WidgetView({
         borderRadius: 16,
       }}
     >
-      <ListWidget style={{ height: "match_parent", width: "match_parent" }}>
-        <TextWidget
-          text={title}
-          clickAction="OPEN_MAIN"
-          style={{
-            color: "#9B9B9B",
-            fontSize: 10,
-            marginBottom: 4,
-            marginLeft: 12,
-            marginTop: 12,
-          }}
-        />
-        {items.map((item, index) => (
+      <OverlapWidget style={{ height: "match_parent", width: "match_parent" }}>
+        <ListWidget style={{ height: "match_parent", width: "match_parent" }}>
           <TextWidget
-            key={index}
-            text={item}
-            clickAction="OPEN_MAIN"
+            text={title}
             style={{
-              color: "#E6E6E6",
-              fontSize: 14,
+              color: "#9B9B9B",
+              fontSize: 10,
+              marginBottom: 4,
               marginLeft: 12,
-              marginRight: 12,
-              marginBottom: 12,
+              marginTop: 12,
             }}
           />
-        ))}
-      </ListWidget>
+          {items.map((item, index) => (
+            <TextWidget
+              key={index}
+              text={item}
+              clickAction="OPEN_MAIN"
+              style={{
+                color: "#E6E6E6",
+                fontSize: 14,
+                marginLeft: 12,
+                marginRight: 12,
+                marginBottom: 12,
+              }}
+            />
+          ))}
+        </ListWidget>
+
+        <FlexWidget
+          style={{
+            height: "match_parent",
+            width: "match_parent",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
+          <FlexWidget
+            style={{
+              width: "match_parent",
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              padding: 12,
+            }}
+          >
+            <FlexWidget
+              clickAction="REFRESH"
+              style={{
+                backgroundColor: "#202020",
+                borderRadius: 24,
+                padding: 12,
+                marginRight: 10,
+              }}
+            >
+              <TextWidget
+                text="🔄"
+                style={{ fontSize: 18, color: "#E6E6E6" }}
+              />
+            </FlexWidget>
+            <FlexWidget
+              clickAction="OPEN_INPUT"
+              style={{
+                backgroundColor: "#202020",
+                borderRadius: 24,
+                padding: 12,
+              }}
+            >
+              <TextWidget
+                text="✏️"
+                style={{ fontSize: 18, color: "#E6E6E6" }}
+              />
+            </FlexWidget>
+          </FlexWidget>
+        </FlexWidget>
+      </OverlapWidget>
     </FlexWidget>
   );
 }
